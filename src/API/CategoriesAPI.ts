0367c3c7008c3a9d95000b73f2c780ad5610655f
Zhuravel Nikita <zhuravel.nikita@gmail.com>
2022-12-29 08:06:31 +0300

    Fixed public directory for Vercel

--- src/API/CategoriesAPI.ts
+++ src/API/CategoriesAPI.ts
@@ -1,13 +1,12 @@
 export default class CategoriesAPI {
   static async getDirections() {
-    const data = await fetch('./src/assets/data/directions.json')
+    const data = await fetch('./data/directions.json')
 
     return data.json()
   }
 
   static async getFilter() {
-    const data = await fetch('./src/assets/data/filter.json')
-    // const data = await fetch('./public/data/filter.json')
+    const data = await fetch('./data/filter.json')
 
     return data.json()
   }
