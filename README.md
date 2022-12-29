a882191c4d53df80eb6e740ea2d382e7e1222972
Zhuravel Nikita <zhuravel.nikita@gmail.com>
2022-12-29 07:58:12 +0300

    Added README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Test
+
+## 👀 Demo
+
+deployed project - **[test](https://main-test-liard.vercel.app/)**
--- src/API/CategoriesAPI.ts
+++ src/API/CategoriesAPI.ts
@@ -1,12 +1,13 @@
 export default class CategoriesAPI {
   static async getDirections() {
-    const data = await fetch('./public/data/directions.json')
+    const data = await fetch('./src/assets/data/directions.json')
 
     return data.json()
   }
 
   static async getFilter() {
-    const data = await fetch('./public/data/filter.json')
+    const data = await fetch('./src/assets/data/filter.json')
+    // const data = await fetch('./public/data/filter.json')
 
     return data.json()
   }
